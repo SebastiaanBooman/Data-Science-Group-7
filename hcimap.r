efef2fd4b23c562c886a9d378e91147e380da170
Bastiaan Teeuwen <bastiaan@mkcl.nl>
2023-09-28 11:17:48 +0200

    fix year filter always evaluating to true

--- hcimap.r
+++ hcimap.r
@@ -1,16 +1,16 @@
 ## Load and install missing packages
-pkgs <- c("readxl", "dplyr", "ggplot2", "rnaturalearth")
+pkgs <- c("readxl", "dplyr", "ggplot2", "rnaturalearth", "rnaturalearthdata")
 newpkgs <- pkgs[!(pkgs %in% installed.packages()[, "Package"])]
 if (length(newpkgs) > 0)
   install.packages(newpkgs)
 lapply(pkgs, library, character.only = TRUE)
 
-year <- "2017"
+target_year <- "2017"
 
 ## Import and clean PWT
 data <-
   read_xlsx("pwt1001.xlsx", sheet = "Data") %>%
-  filter(year == year) %>%
+  filter(year == target_year) %>%
   select(c("countrycode", "hc"))
 
 ## Load and clean world map data
@@ -27,7 +27,10 @@ hcimap <-
   geom_sf(color = "white", linewidth = 0.1) +
   scale_fill_gradientn(colors = rev(terrain.colors(10))) +
   theme_bw() +
-  labs(title = paste("Human Captial Index (HCI) in", year), fill = "HCI") +
+  labs(
+    title = paste("Human Captial Index (HCI) in", target_year),
+    fill = "HCI"
+  ) +
   theme(
     legend.position  = "bottom",
     legend.box       = "horizontal",
