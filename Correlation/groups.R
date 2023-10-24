cd418c026ff28f63b7a6952a8e67c1612a5a5ca4
Bastiaan Teeuwen <bastiaan@mkcl.nl>
2023-10-24 14:56:04 +0200

    correlation: code cleanup

--- Correlation/groups.R
+++ Correlation/groups.R
@@ -1,26 +1,29 @@
 source("Correlation/utils.r")
 pacman::p_load(rnaturalearth, stringr, sf, reshape)
 
-SAVE_COR_RES <- FALSE
+vars <- list(
+  pop    = "Population",
+  avh    = "Average hours worked",
+  cshm_i = "Capital formation",
+  ck     = "Capital services",
+  hc     = "HCI",
+  ctfp   = "TFP"
+)
 
 gen_results <- function(data, out) {
   data <- data %>%
     group_by(year) %>%
     summarize_if(is.numeric, mean)
 
-  gdp <- "cgdpo"
-  name <- "GDP"
+  ## Map every variable in vars and correlate against GDP,
+  ## the results are stored in a list.
+  results <- mapply(function(var, name) {
+    correlate(data, "cgdpo", var, "GDP", name, out, FALSE)
+  }, names(vars), vars, SIMPLIFY = FALSE, USE.NAMES = FALSE)
 
-  return(
-    list(
-      "Population" = correlate(data, gdp, "pop", name, "Population", out, SAVE_COR_RES),
-      "Capital formation" = correlate(data, gdp, "cshm_i", name, "Capital formation", out, SAVE_COR_RES),
-      "capital services" = correlate(data, gdp, "ck", name, "Capital services", out, SAVE_COR_RES),
-      "Average hours worked" = correlate(data, gdp, "avh", name, "Average hours worked", out, SAVE_COR_RES),
-      "TFP" = correlate(data, gdp, "ctfp", name, "TFP", out, SAVE_COR_RES),
-      "HCI" = correlate(data, gdp, "hc", name, "HCI", out, SAVE_COR_RES)
-    )
-  )
+  names(results) <- unlist(vars)
+
+  return(results)
 }
 
 ## Gather country economy status data, geometry data is dropped
@@ -32,7 +35,6 @@ ne <- ne_countries(returnclass = "sf") %>%
 
 ## Prepare the dataset for further processing
 penn <- pwt() %>%
-  filter(year >= 1990 & year <= 2019) %>%
   mutate(
     cshm_c = cgdpo * csh_c,
     cshm_i = cgdpo * csh_i,
@@ -48,41 +50,33 @@ results <- list(
     filter(economy == "Developed region: G7") %>%
     left_join(penn, by = join_by(countrycode)) %>%
     gen_results("Developed Countries"),
-  "Dveloping countries" = ne %>%
+  "Developing countries" = ne %>%
     filter(economy == "Developing region") %>%
     left_join(penn, by = join_by(countrycode)) %>%
     gen_results("Developing Countries"),
 
   USA = penn %>% filter(countrycode == "USA") %>% gen_results("USA"),
   Canada = penn %>% filter(countrycode == "CAN") %>% gen_results("Canada"),
-  China = penn %>% filter(countrycode == "CHN") %>% gen_results("China")
+  China = penn %>% filter(countrycode == "CHN") %>% gen_results("China"),
+  Russia = penn %>% filter(countrycode == "RUS") %>% gen_results("Russia")
 )
 
 ## Generate a correlation matrix
-independent_vars <- c("cgdpo", "pop", "cshm_i", "ck", "avh", "ctfp", "hc")
-
 m <- matrix(unlist(results), ncol = length(results), byrow = TRUE)
 colnames(m) <- names(results)
 rownames(m) <- names(results[[1]])
 
-# TODO generate fancy matrix with ggplot2
-
-#set.seed(8)
-#ma <- matrix(round(rnorm(200), 2), 10, 10)
-#colnames(ma) <- paste("Col", 1:10)
-#rownames(ma) <- paste("Row", 1:10)
-
-# Transform the matrix in long format
+## Transform the matrix into a "long format"
 df <- melt(m)
 colnames(df) <- c("x", "y", "value")
 
 ggplot(df, aes(x = , x, y = y, fill = value)) +
   labs(
-    title = "Correlation matrix",
-    #subtitle = subtitle,
-    x = "variable",
-    y = "RGDPo"
+    title = "Correlation matrix - GDP vs. Variable",
+    x = "Variable",
+    y = "GDP"
   ) +
   geom_tile() +
-  geom_text(aes(x,y, label=value), color = ifelse(df$value > 0.5, "white", "black"), size = 5) +
-  scale_fill_distiller(palette = 2, direction=1)
+  geom_text(aes(x, y, label = value),
+            color = ifelse(df$value > 0.5, "white", "black"), size = 5) +
+  scale_fill_distiller(palette = 2, direction = 1)
