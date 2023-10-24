2812301470e785ec10cf636eea4ce250448c2cad
Sebastiaan Booman <iMeowTwitch@gmail.com>
2023-10-24 20:38:48 +0200

    Updated utils

--- Correlation/utils.R
+++ Correlation/utils.R
@@ -1,4 +1,5 @@
 source("common.R")
+source("./Correlation/statistical_tests.R")
 pacman::p_load(ggplot2)
 
 presentation_theme <- theme(
@@ -129,10 +130,16 @@ save_correlation_plots <- function(lin_model, dependent_name, independent_name,
   )
   
   ## Normality plots
+  # FIXME: ew, inefficient, but who cares
+  sw_test <- shapiro.test(lin_model$.resid) #TODO: MOVE THIS TO PARAMETER!
   qq_p <- plot_cor_test(
     dataframe = data.frame(qqnorm(lin_model$.stdresid, plot.it = FALSE)[[1]],
                            lin_model$.stdresid),
     #title = paste("Q-Q Plot:", title),
+    subtitle = paste(
+      "SW stat =", round(sw_test$statistic, 2),
+      "SW P = ", round(sw_test$p.value,2)
+    ),
     xlab = "Theoretical Quantiles",
     ylab = "Standardized Residuals",
     geom_point = TRUE,
@@ -156,90 +163,6 @@ save_correlation_plots <- function(lin_model, dependent_name, independent_name,
     qq_p
   )
 }
-
-
-mean_of_residuals_test <- function(rdl_vector, res_thresh) {
-  res_mean <- mean(rdl_vector)
-  
-  return(data.frame(test = c("Mean of residuals close to 0"),
-                    result = c(as.character(res_mean)),
-                    pass = c(as.character(res_mean <= res_thresh))
-                    )
-         )
-    
-}
-
-hypothesis_test <- function(responses, terms, method = "pearson",
-                            significance_level = 0.05) {
-  confidence_level <- 1 - significance_level
-  strong_threshold <- 0.75 #TODO: Figure out threshold
-  
-  ## Mean of residuals check
-  c_test <- cor.test(responses, terms, method = method,
-                   conf.level = confidence_level)
-  
-  #Output 1
-  conf_interval <- c(paste(confidence_level * 100, "% confidence interval", sep = ""),
-    paste(c_test$conf.int[1], c_test$conf.int[2], sep = " - "), 
-    "NA")
-  
-  #Output 2
-  if (c_test$p.value < 0.001) evidence_against_h0 <- "Strong evidence"
-  else if (c_test$p.value < 0.05) evidence_against_h0 <- "Moderate evidence"
-  else if (c_test$p.value < 0.1) evidence_against_h0 <- "Weak evidence"
-  else if (c_test$p.value >= 0.1) evidence_against_h0 <- "Insufficient evidence"
-  p_value <- c(
-    "Reject H₀ with p-value", 
-    as.character(c_test$p.value),
-    evidence_against_h0)
-  #Output 3
-  test_estimate <- c(paste(method, " correlation coefficient >= (-)", strong_threshold, sep = ""),
-    as.character(c_test$estimate),
-    as.character(c_test$estimate >= strong_threshold || c_test$estimate <= -strong_threshold))
-  
-  return(data.frame(
-    test = c(
-      conf_interval[1],
-      p_value[1],
-      test_estimate[1]
-    ),
-    result = c(
-      conf_interval[2],
-      p_value[2],
-      test_estimate[2]
-    ),
-    
-    pass = c(
-      conf_interval[3],
-      p_value[3],
-      test_estimate[3]
-    )
-  ))
-}
-
-shapiro_wilk_test <- 
-
-## Aggregate correlation test results and save to a csv file
-save_correlation_stats <- function(lin_model, output_dir) {
-  mean_residual_thresh <- 0.005 #TODO: Why this threshold?
-  
-  mean_of_resid_test <- mean_of_residuals_test(lin_model$.resid, mean_residual_thresh)
-  hypo_test <- hypothesis_test(lin_model$response, lin_model$terms)
-  shapiro_test <- 
-  
-  
-  results <- bind_rows(mean_of_resid_test, hypo_test)
-  
-  # TODO: Independence X var and residuals check
-  
-  ## Write the results to a CSV file
-  filename <- paste(output_dir, "stat_tests.csv", sep = "/", collapse = "/")
-  write.csv(results, filename, row.names = FALSE)
-  
-  # TODO: add summary output to (a) csv file
-  #summary <- summary(lin_model)
-}
-
 
 ## Convenience function to set up a basic linear model and write the results
 ## and plots to disk
