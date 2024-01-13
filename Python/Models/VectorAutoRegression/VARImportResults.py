999fde630f80f5c7483050d5d9bbfd2651677206
Sebastiaan Booman <iMeowTwitch@gmail.com>
2024-01-13 15:47:42 +0100

    Add % country amount plot func

--- Python/Models/VectorAutoRegression/VARImportResults.py
+++ Python/Models/VectorAutoRegression/VARImportResults.py
@@ -5,27 +5,35 @@ import matplotlib.pyplot as plt
 import math
 
 class VARImportResults:
-    def load_and_plot_var_res(path: str, stat_to_plot: str, title: str, ylab: str, xlab_lambda, log:bool):
+    def load_and_plot_var_res(path: str, stat_to_plot: str, title: str, ylab: str, xlab_lambda, percentage:bool, log:bool):
         res = ExportVARResults.load(path)
 
         res = VARExportClass(**res)
         filename = path.replace(".json", "")
-        VARImportResults.plot(res, stat_to_plot, title, ylab, xlab_lambda, filename, log)
+        VARImportResults.plot(res, stat_to_plot, title, ylab, xlab_lambda, percentage, filename, log)
     
-    def plot(obj: VARExportClass, stat_to_plot: str, title: str, ylab: str, xlab_lambda, filename: str, log:bool):
+    def plot(obj: VARExportClass, stat_to_plot: str, title: str, ylab: str, xlab_lambda, percentage, filename: str, log:bool):
         folds = [i for i in range(len(obj.dev_status_results[0]["fold_results"]))]
         dev_statuses = [xlab_lambda(ds) for ds in obj.dev_status_results]
 
         total_fold_res = {}
         
         for fold_i in range(len(folds)):
-            i_fold_rmse = []
+            i_fold_res = []
             for dev_status_res in obj.dev_status_results:
+                total_country_amount = dev_status_res["country_amount"]
                 for fold_res in dev_status_res["fold_results"]:
                     if fold_res["fold_ita"] == (fold_i + 1):
-                        i_fold_rmse.append(round(fold_res[stat_to_plot], 3) if type(fold_res[stat_to_plot]) in (float, int) else 0)
+                        if percentage:                               
+                            i_fold_res.append(
+                                round((fold_res[stat_to_plot]/total_country_amount) * 100, 1) if type(fold_res[stat_to_plot]) in (float, int) else 0 
+                            )
+                        else:
+                            i_fold_res.append(
+                                round(fold_res[stat_to_plot], 3) if type(fold_res[stat_to_plot]) in (float, int) else 0
+                            )
                 
-            total_fold_res[f"Fold {fold_i + 1}"] = i_fold_rmse #[dev_status["fold_results"] for dev_status in obj.dev_status_results if dev_status["fold_results"]["fold_ita"] == i]
+            total_fold_res[f"Fold {fold_i + 1}"] = i_fold_res 
 
         x = np.arange(len(dev_statuses))  # the label locations
         width = 0.20  # the width of the bars
@@ -44,7 +52,6 @@ class VARImportResults:
             ax.bar_label(rects, padding=3)
             multiplier += 1
 
-        # Add some text for labels, title and custom x-axis tick labels, etc.
         if log:
             title = title + ' (Log10)'
             ylab = ylab + ' (Log10)'
@@ -57,13 +64,53 @@ class VARImportResults:
         #ax.set_ylim(0, 250)
         #plt.show()
 
-        plt.savefig(f"{filename} + {stat_to_plot}.png", dpi=150)
+        plt.savefig(f"{filename} + {stat_to_plot}_percentage.png", dpi=150)
 
 if __name__ == "__main__":
     log:bool = True
+    percentage:bool = True
 
-    VARImportResults.load_and_plot_var_res("./VAR dev status results.json", "mean_rmse", 'VAR RMSE by fold per development status', "RMSE", lambda x: x["development_status"], log)
-    VARImportResults.load_and_plot_var_res("./VAR dev status results.json", "data_amount", 'VAR country amount by fold per development status', "Data amount", lambda x: f"{x['development_status']}\n(total country amount: {x['country_amount']})", False)
+    # Parameter tuning results export
+    """
+    VARImportResults.load_and_plot_var_res(
+        "./VAR dev status results.json",
+        "mean_rmse",
+        'VAR RMSE by fold per development status',
+        "RMSE",
+        lambda x: x["development_status"], 
+        False,
+        log
+    )"""
+    VARImportResults.load_and_plot_var_res(
+        "./VAR dev status results.json",
+        "data_amount",
+        '% VAR country amount by fold per development status',
+        "% Data amount",
+        lambda x: f"{x['development_status']}\n(total country amount: {x['country_amount']})", 
+        percentage,
+        False
+    )
 
-    VARImportResults.load_and_plot_var_res("./Baseline_VAR dev status results.json", "mean_rmse", 'VAR RMSE by fold per development status', "RMSE", lambda x: x["development_status"], log)
-    VARImportResults.load_and_plot_var_res("./Baseline_VAR dev status results.json", "data_amount", 'VAR country amount by fold per development status', "Data amount", lambda x: f"{x['development_status']}\n(total country amount: {x['country_amount']})", False)
+"""
+
+
+    # Baseline model results export
+    VARImportResults.load_and_plot_var_res(
+        "./Baseline_VAR dev status results.json",
+        "mean_rmse",
+        'VAR RMSE by fold per development status',
+        "RMSE",
+        lambda x: x["development_status"], 
+        False,
+        log
+    )
+    VARImportResults.load_and_plot_var_res(
+        "./Baseline_VAR dev status results.json",
+        "data_amount",
+        'VAR country amount by fold per development status',
+        "Data amount",
+        lambda x: f"{x['development_status']}\n(total country amount: {x['country_amount']})",
+        percentage,
+        False
+    )
+"""
